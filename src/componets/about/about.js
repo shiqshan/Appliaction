b8b388cb28ad0386d1697d7ec509d42018b9c911
95605 <956059246@qq.com>
2022-08-10 20:09:49 +0800

    测试

--- src/componets/about/about.js
+++ src/componets/about/about.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 
 class About extends Component {
+
+    state = {
+        count: 0
+    }
+
+    handle = (e) => {
+        e.
+        this.setState({count: this.state.count + 1})
+        this.setState({count: this.state.count + 1})
+        this.setState({count: this.state.count + 1})
+        console.log(e)
+    }
+
     render() {
         return (
             <div>
-                About
+                {/* <span>行元素</span>*/}
+                {/*<div className={'kkk'}>*/}
+                {/*    <div className={'hhh'}></div>*/}
+                {/*</div>*/}
+                {/*<div className={'fixed'}></div>*/}
+                <div className={'kkk'} onClick={ e=> this.handle}>
+                    <div className={'hhh'} onClick={e => {
+                        // e.stopPropagation()
+                        console.log('我是里面的')
+                    }}></div>
+                </div>
             </div>
         );
     }
